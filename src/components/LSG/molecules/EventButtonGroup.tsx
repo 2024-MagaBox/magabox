24816da880bb8d9387e61a05fd09e177753e6a46
LSG <sukyeng97@naver.com>
2024-08-20 23:03:02 +0900

    subnavbar

---
+++ src/components/LSG/molecules/EventButtonGroup.tsx
@@ -0,0 +1,14 @@
+import Button from "../atom/Button";
+
+const EventButtonGroup = () => {
+  return (
+    <>
+      <Button title="전체" />
+      <Button title="미소지기Pick" />
+      <Button title="영화" />
+      <Button title="제휴/할인" />
+    </>
+  );
+};
+
+export default EventButtonGroup;
--- src/components/LSG/organism/SubNavbar.tsx
+++ src/components/LSG/organism/SubNavbar.tsx
@@ -1,10 +1,9 @@
+import EventButtonGroup from "../molecules/EventButtonGroup";
+
 const SubNavbar = () => {
   return (
     <div className="w-full px-5 bg-subColor text-white flex">
-      <div className="px-10 py-3 hover:cursor-pointer">전체</div>
-      <div className="px-10 py-3 hover:cursor-pointer">미소지기Pick</div>
-      <div className="px-10 py-3 hover:cursor-pointer">영화</div>
-      <div className="px-10 py-3 hover:cursor-pointer">제휴/할인</div>
+      <EventButtonGroup />
     </div>
   );
 };
