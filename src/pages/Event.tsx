6097bc5d206da0cabd8356512dd1eb847c907dcc
LSG <sukyeng97@naver.com>
2024-08-21 23:21:34 +0900

    todo

--- src/pages/Event.tsx
+++ src/pages/Event.tsx
@@ -18,9 +18,61 @@ const Event = () => {
           <ArrowForwardIosRoundedIcon className="hover:cursor-pointer" />
         </div>
 
-        {/* Bottom Content 스크롤 어케하는거지... */}
+        {/* Bottom Content x축 스크롤 어케하는거지... */}
         <div className="py-7 text-lg font-bold">미소지기 Pick</div>
-        <div className="flex gap-10 overflow-auto snap-x snap-proximity">
+        <div className="flex gap-10 overflow-auto snap-x snap-proximity mb-20">
+          <EventCard
+            imgsrc="/img/eventLinkImg-1.jpg"
+            title="[프로모션][굿즈] <메가박스X위글위글> 콤보 런칭!"
+            period="2024.08.21 ~ 2024.10.30"
+          />
+          <EventCard
+            imgsrc="/img/eventLinkImg-2.jpg"
+            title="메가박스 오리지널 슬라이드 No.1 <본 아이덴티티>"
+            period="2024.08.20 ~ 2024.09.20"
+          />
+          <EventCard
+            imgsrc="/img/eventLinkImg-3.jpg"
+            title="[라이브뷰잉] hololive English 2nd Concert"
+            period="2024.08.25 ~ 2024.08.26"
+          />
+          <EventCard
+            imgsrc="/img/eventLinkImg-4.jpg"
+            title="<룩백> 프리미어 상영회"
+            period="2024.08.31 ~ 2024.09.01"
+          />
+        </div>
+
+        {/*  */}
+        <div className="py-7 text-lg font-bold">영화</div>
+        <div className="flex gap-10 overflow-auto snap-x snap-proximity mb-20">
+          <EventCard
+            imgsrc="/img/eventLinkImg-1.jpg"
+            title="[프로모션][굿즈] <메가박스X위글위글> 콤보 런칭!"
+            period="2024.08.21 ~ 2024.10.30"
+          />
+          <EventCard
+            imgsrc="/img/eventLinkImg-2.jpg"
+            title="메가박스 오리지널 슬라이드 No.1 <본 아이덴티티>"
+            period="2024.08.20 ~ 2024.09.20"
+          />
+          <EventCard
+            imgsrc="/img/eventLinkImg-3.jpg"
+            title="[라이브뷰잉] hololive English 2nd Concert"
+            period="2024.08.25 ~ 2024.08.26"
+          />
+          <EventCard
+            imgsrc="/img/eventLinkImg-4.jpg"
+            title="<룩백> 프리미어 상영회"
+            period="2024.08.31 ~ 2024.09.01"
+          />
+        </div>
+
+        {/* subnav에서 극장 눌렀을때 스크롤이 여기로 오도록하기 */}
+        <a href="#theater" className="py-7 text-lg font-bold">
+          극장
+        </a>
+        <div className="flex gap-10 overflow-auto snap-x snap-proximity mb-20">
           <EventCard
             imgsrc="/img/eventLinkImg-1.jpg"
             title="[프로모션][굿즈] <메가박스X위글위글> 콤보 런칭!"
