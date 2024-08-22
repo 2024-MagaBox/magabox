7d4350f8b73e4c1ef0bc6939a08e6cd86ca0f6f8
LSG <sukyeng97@naver.com>
2024-08-22 23:11:32 +0900

    header 수정

--- src/components/LSG/organism/SubNavbar.tsx
+++ src/components/LSG/organism/SubNavbar.tsx
@@ -1,9 +1,12 @@
-import EventButtonGroup from "../molecules/EventButtonGroup";
+import Button from "../atom/Button";
 
 const SubNavbar = () => {
   return (
     <div className="w-full fixed top-24 px-5 bg-subColor text-white flex">
-      <EventButtonGroup />
+      <Button title="전체" />
+      <Button title="미소지기Pick" />
+      <Button title="영화" />
+      <Button title="제휴/할인" />
     </div>
   );
 };
