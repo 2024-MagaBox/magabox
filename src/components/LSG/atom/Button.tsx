ceecfd607cf5a771621c602996c02e72d402e759
LSG <sukyeng97@naver.com>
2024-08-26 22:22:25 +0900

    button

--- src/components/LSG/atom/Button.tsx
+++ src/components/LSG/atom/Button.tsx
@@ -1,7 +1,7 @@
 type ButtonProps = { title?: string };
 
 const Button = ({ title = "title" }: ButtonProps) => {
-  return <div className="px-10 py-3 hover:cursor-pointer">{title}</div>;
+  return <a className="px-10 py-3 hover:cursor-pointer">{title}</a>;
 };
 
 export default Button;
