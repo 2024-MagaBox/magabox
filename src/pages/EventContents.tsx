3c4cce07be60798d7d9e88a93abfd55f9e4de70b
leessungkyu <lkyu63@naver.com>
2024-09-30 00:02:03 +0900

    20240929

--- src/pages/EventContents.tsx
+++ src/pages/EventContents.tsx
@@ -4,16 +4,39 @@ import { Params, useParams } from "react-router-dom";
 import HorizontalLine from "../components/LSG/atom/HorizontalLine";
 
 const EventContents = () => {
-  const { eventContentsId } = useParams(); // path의 :params명과 동일
-  const [eventContent, setEventContent] = useState({});
+  const { EVENT_ID } = useParams<Params>();
+
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [begin, setBegin] = useState<Date>();
+  const [end, setEnd] = useState<Date>();
+
+  // 제목
+  useEffect(() => {
+    axios.get(`http://localhost:8080/api/event-title/${EVENT_ID}`).then((res) => {
+      setTitle(res.data);
+    });
+  }, []);
+
+  // 이미지
+  useEffect(() => {
+    axios.get(`http://localhost:8080/api/event-contents/${EVENT_ID}`).then((res) => {
+      setContent(res.data);
+    });
+  }, []);
+
+  //기간
+  useEffect(() => {
+    axios.get(`http://localhost:8080/api/event-begin/${EVENT_ID}`).then((res) => {
+      setBegin(res.data);
+    });
+  }, []);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const request = await axios.get(`/event/${eventContentsId}`); // axios에서 활용
-      setEventContent(request.data);
-    };
-    fetchData();
-  }, [eventContentsId]);
+    axios.get(`http://localhost:8080/api/event-end/${EVENT_ID}`).then((res) => {
+      setEnd(res.data);
+    });
+  }, []);
 
   return (
     <>
